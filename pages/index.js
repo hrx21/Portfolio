2da2d0158b89fee9288b4ee4841b3cee6995fa60
Ninad Muranjan <hrxprog69@gmail.com>
2022-12-02 16:48:21 +0530

    hfuyrehd

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,15 @@ import design from '../public/images/design.png'
 import figma from '../public/images/figma.png'
 import canva from '../public/images/canva.png'
 import code from '../public/images/code.png'
+import html from '../public/images/html.png'
+import css from '../public/images/css1.png'
+import js from '../public/images/js1.png'
+import next from '../public/images/next.png'
+import mui from '../public/images/mui.png'
+import react from '../public/images/react.png'
+import git from '../public/images/git.png'
+import bootstrap from '../public/images/bootstrap.png'
+import IndividualIntervalsExample from './carousel';
 
 export default function Home() {
   return (
@@ -81,16 +90,23 @@ export default function Home() {
                 <h3 className='text-lg font-medium pt-8 pb-2'>Technologies I use</h3>
                 <p className='py-2'>Creating Elegant websites and applications suited for your needs.</p>
                 <h4 className='text-teal-500 py-4'>Languages I use.</h4>
-                <p className='text-gray-700 py-1'>Figma</p>
-                <p className='text-gray-700 py-1'>Canva</p>
+                <div className='flex justify-center items-center flex-row flex-1 gap-10'>
+                  <Image src={html} width={100} height={100} />
+                  <Image src={css} width={100} height={100} />
+                  <Image src={js} width={100} height={100} />
+                  <Image src={react} width={100} height={100} />
+                  <Image src={next} width={100} height={100} />
+                  <Image src={git} width={100} height={100} />
+                  <Image src={bootstrap} width={100} height={100} />
+                  <Image src={mui} width={100} height={100} />
+                </div>
               </div>
-
-              <div className='flex items-center justify-center flex-col shadow-xl p-10 rounded-xl my-10'>
+              {/* <div className='flex items-center justify-center flex-col shadow-xl p-10 rounded-xl my-10'>
                 <Image src={design} width={100} height={100} />
                 <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
                 <p className='py-2'>Creating Elegant designs suited for your needs.</p>
                 <h4 className='text-teal-500 py-4'>Design Tools I use.</h4>
-              </div>
+              </div> */}
             </div>
           </section>
         </main>
